530fc741dd0fda94c54db3c1236723fadf9dd77f
shanmugam_manikandan <shanmugam.manikandan@cognizant.com>
2024-12-05 15:10:19 +0800

    update the css styles

--- src/components/Agenta.tsx
+++ src/components/Agenta.tsx
@@ -9,8 +9,8 @@ const Agenda = () => {
                 Program
             </div>
             <div style={{ fontFamily: "Franklin" }} className='bg-white rounded-lg shadow-lg'>
-                <p style={{ fontFamily: "Franklin" }} className='md:text-center p-6 md:p-16 pb-4 md:text-2xl text-xl font-normal'>A fireside chat with a woman adorned in world championship glory, Compelling speeches and Evaluations from the best in District 80, Carefully curated speechcraft, A regal gala dinner, and plenty in the way of culinary delights.</p>
-                <p style={{ fontFamily: "Franklin" }} className='text-center md:text-center pt-2 text-2xl'>We are awaiting you.</p>
+                <p style={{ fontFamily: "Franklin" }} className='md:text-center p-6  pb-4  text-xl font-normal'>A fireside chat with a woman adorned in world championship glory, compelling speeches and evaluations from the best in District 80, carefully curated speechcraft, a regal gala dinner, and plenty in the way of culinary delights.</p>
+                <p style={{ fontFamily: "Franklin" }} className='text-center md:text-center text-2xl'>We are awaiting you.</p>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full mx-auto p-8">
                     <div className="flex flex-col pb-8 mb-4 md:border-b-0 border-dashed border-b-2 border-gray-300">
                         <h3 className="text-2xl font-bold mb-4 text-center">9th May 2025</h3>
